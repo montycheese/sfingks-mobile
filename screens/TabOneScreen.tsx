eabd40b969b35c79268607c56b3cd62e6400a7f4
Montana Wong <montanawong@gmail.com>
2020-08-31 00:14:20 -0700

    Chat WIP

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,64 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import {LinearGradient} from "expo-linear-gradient";
+import { GiftedChat } from 'react-native-gifted-chat'
+import {useEffect, useState} from "react";
 
 export default function TabOneScreen() {
+
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+            {
+                _id: 2,
+                text: 'Hello back',
+                user: {
+                    _id: 1,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ])
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <LinearGradient
+          // Background Linear Gradient
+          colors={['#FF124F', '#FF00A0', '#FE75FE', '#7A04EB', '#120458']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            height: '100%'
+          }}
+      />
+      <View style={styles.chatContainer}>
+          <GiftedChat
+              messages={messages}
+              onSend={console.log}
+              user={{
+                  _id: 1,
+              }}
+              showUserAvatar={false}
+              renderAvatar={() => null}
+              placeholder="Tell us something..."
+              alignTop={true}
+          />
+      </View>
     </View>
   );
 }
@@ -17,8 +66,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  chatContainer: {
+      flex: 1,
+      backgroundColor: 'black',
+      opacity: 0.8,
+      marginLeft: '5%',
+      marginRight: '5%',
+      marginTop: '10%',
   },
   title: {
     fontSize: 20,
